d60062789b4bc9d845e23f92a6ef04ab2d0308ec
Kazuki Okimori <kz.ok.0116@gamil.com>
2020-10-20 23:11:31 +0900

    オブジェクト型を扱う場合

--- sample.ts
+++ sample.ts
@@ -1,2 +1,15 @@
 let hello: string = 'hello';
 console.log(hello);
+
+
+
+
+
+// オブジェクトについて
+const user: { name: string, age: number} = {
+  name: "ebihara",
+  age: 44,
+};
+console.log(user.name); // ebihara
+console.log(user.aga); // エラー： プロパティ名が間違っている
+console.log(user.age.length); // エラー： age は number 型で length プロパティはない
